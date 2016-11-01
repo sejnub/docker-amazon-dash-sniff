d5efc0dafc78c3fcbc862c1bd33bf1be1723bead
Heiner Bunjes <heiner@bunjes.net>
2016-11-01 01:07:29 +0100

    Update dash1.py

--- dash1.py
+++ dash1.py
@@ -18,6 +18,10 @@ def button_pressed_dash3():
   current_time = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
   print 'Dash button caffe-1 pressed at ' + current_time
  
+def button_pressed_dash4():
+  current_time = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
+  print 'Dash button 00:e2:66:59:02:9a pressed at ' + current_time
+ 
 def udp_filter(pkt):
   options = pkt[DHCP].options
   for option in options:
@@ -27,9 +31,19 @@ def udp_filter(pkt):
        mac_to_action[pkt.src]()
        break
  
+# My buttons
+# somat-1 ac:63:be:e3:3c:64 192.168.178.75
+# ariel-1 ac:63:be:44:51:b3 
+# caffe-1 ac:63:be:a3:5c:03 192.168.178.74
+ 
+
+
+
 mac_to_action = {'ac:63:be:e3:3c:64' : button_pressed_dash1, 
                  'ac:63:be:44:51:b3' : button_pressed_dash2, 
-                 'ac:63:be:a3:5c:03' : button_pressed_dash3}
+                 'ac:63:be:a3:5c:03' : button_pressed_dash3,
+                 '00:e2:66:59:02:9a' : button_pressed_dash4
+                }
 mac_id_list = list(mac_to_action.keys())
  
 print "Running dash.py version 3"
