e90e78f0a05addd0191e836d730cb0ea2864f7c1
Heiner Bunjes <heiner@bunjes.net>
2016-11-04 20:21:27 +0100

    Update dash2.py

--- dash2.py
+++ dash2.py
@@ -9,12 +9,16 @@ from scapy.all import *
 def arp_display(pkt):
   if pkt[ARP].op == 1: #who-has (request)
     if pkt[ARP].hwsrc == 'ac:63:be:a3:5c:03': # button 1
-        print "Pushed Coffee"
+        print "Pushed caffe-1"
+    if pkt[ARP].hwsrc == 'ac:63:be:44:51:b3': # button 1
+        print "Pushed ariel-1"
+    if pkt[ARP].hwsrc == 'ac:63:be:e3:3c:64': # button 1
+        print "Pushed somat-1"
     else:
         print "ARP Probe from unknown device: " + pkt[ARP].hwsrc
 
 
-print "Running dash.py version 4"
+print "Running dash2.py version 5
 print "Waiting for an amazon dash button to register to the network ..."
 
 sniff(prn=arp_display, filter="arp", store=0, count=0)
