d3b297c88f4367525bad1f9a3e80131068c03187
Heiner Bunjes <heiner@bunjes.net>
2016-11-05 00:57:21 +0100

    Update dash2.py

--- dash2.py
+++ dash2.py
@@ -9,15 +9,20 @@ from scapy.all import *
 def arp_display(pkt):
   if pkt.haslayer(ARP):
     if pkt[ARP].op == 1: #who-has (request)
-      if pkt[ARP].hwsrc == 'ac:63:be:a3:5c:03': # button 1
-        print "Pushed caffe-1"
-      if pkt[ARP].hwsrc == 'ac:63:be:44:51:b3': # button 1
-        print "Pushed ariel-1"
-      if pkt[ARP].hwsrc == 'ac:63:be:e3:3c:64': # button 1
-        print "Pushed somat-1"
-      #else:
-        #print "ARP Probe from unknown device: " + pkt[ARP].hwsrc
-
+      if pkt[ARP].hwsrc == 'ac:63:be:a3:5c:03': 
+        button = "caffe-1"
+      if pkt[ARP].hwsrc == 'ac:63:be:44:51:b3': 
+        button = "ariel-1"
+      if pkt[ARP].hwsrc == 'ac:63:be:e3:3c:64': 
+        button = "somat-1"
+      else:
+        button = "unknown"
+        
+  if button == "unknown:
+    print "ARP Probe from unknown device: " + pkt[ARP].hwsrc
+  else:
+    print "Pushed button: " + button
+    
 
 print "Running dash2.py version 5"
 print "Waiting for an amazon dash button to register to the network ..."
@@ -26,3 +31,21 @@ sniff(prn=arp_display, filter="arp", store=0, count=0)
 
 if __name__ == "__main__":
   main()
+
+  
+# python 2.7  
+# import urllib
+# import urllib2
+
+# url = 'http://www.someserver.com/cgi-bin/register.cgi'
+# values = {'name' : 'Michael Foord',
+#           'location' : 'Northampton',
+#           'language' : 'Python' }
+# 
+# data = urllib.urlencode(values)
+# req = urllib2.Request(url, data)
+# response = urllib2.urlopen(req)
+# the_page = response.read()  
+
+
+# eof  
