2204d2012d2ca06a8f91ce1ef3e0721248a8a13b
Heiner Bunjes <heiner@bunjes.net>
2017-07-17 15:42:13 +0200

    Update dash.py

--- dash.py
+++ dash.py
@@ -14,7 +14,7 @@ from scapy.all import *
 
 
 # Constants
-timespan_threshhold = 12
+timespan_threshhold = 15
 
 # The following environment variables are set by the 'docker run' command via the '--env-file' option
 username = os.getenv('HB_FHEMWEB_USERNAME', 'unknown')
