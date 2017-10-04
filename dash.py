eb268c08ca67654eefe44ed16e599b0940ca468d
Heiner Bunjes <heiner@bunjes.net>
2017-10-04 20:29:41 +0200

    Increased the no-repeat-timespan to 35 seconds

--- dash.py
+++ dash.py
@@ -14,7 +14,7 @@ from scapy.all import *
 
 
 # Constants
-timespan_threshhold = 30
+timespan_threshhold = 35
 
 # The following environment variables are set by the 'docker run' command via the '--env-file' option
 username = os.getenv('HB_FHEMWEB_USERNAME', 'unknown')
